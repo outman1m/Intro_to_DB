b1ca772eadcc3cd6e666a60b398ae642309439ac
outman1m <outman.talbi200@gmail.com>
2025-06-27 12:27:53 -0700

    Add script to insert multiple customer records

---
+++ task_6.sql
@@ -0,0 +1,5 @@
+-- Script to insert multiple customers into customer table
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
