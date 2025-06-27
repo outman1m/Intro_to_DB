01fadeba0c8df302d6419b4b6683a49c9c53c1b6
outman1m <outman.talbi200@gmail.com>
2025-06-27 12:24:30 -0700

    Add script to insert first customer data

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+-- Script to insert a single customer into customers table
+INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
